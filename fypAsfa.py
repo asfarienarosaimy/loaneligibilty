a6e47210ebef74a2c31c37c77635b2f9511c86b9
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2025-01-18 02:37:01 +0800

    Update fypAsfa.py

--- fypAsfa.py
+++ fypAsfa.py
@@ -3,40 +3,66 @@ import pandas as pd
 import numpy as np
 import joblib
 
-model= joblib.load('logistic_regression_model.joblib')
+# Load the pre-trained model
+model = joblib.load('logistic_regression_model.joblib')
 
-st.title('Loan Eligibility Prediction:bank:')
+# App Title
+st.title('Loan Eligibility Prediction :bank:')
 
-Gender= st.selectbox('Gender',('Male','Female'))
-Married= st.selectbox('Married',('No','Yes'))
-Dependents= st.selectbox('Number Of Dependents',('0','1','2','3 or More Dependents'))
-Education= st.selectbox('Education status',('Graduate','Not Graduate'))
-Self_Employed= st.selectbox('Self Employed',('No','Yes'))
-ApplicantIncome= st.number_input('Applicant Income',0)
-CoapplicantIncome= st.number_input('Coapplicant Income',0)
-LoanAmount= st.number_input('Loan Amount',0)
-Loan_Amount_Term= st.select_slider('Loan Amount Term',['1 YEAR','3 YEARS','5 YEARS','7 YEARS',
-                                   '10 YEARS','15 YEARS','20 YEARS','25 YEARS','30 YEARS','40 YEARS'])
-Credit_History= st.select_slider('Credit History 1 for Good 0 for Bad',[0,1])
-Property_Area= st.selectbox('Area of Property',('Urban','Rural','Semiurban'))
+# Input Fields
+Gender = st.selectbox('Gender', ('Male', 'Female'))
+Married = st.selectbox('Married', ('No', 'Yes'))
+Dependents = st.selectbox('Number Of Dependents', ('0', '1', '2', '3+'))
+Education = st.selectbox('Education Status', ('Graduate', 'Not Graduate'))
+Self_Employed = st.selectbox('Self Employed', ('No', 'Yes'))
+ApplicantIncome = st.number_input('Applicant Income', min_value=0, step=1000)
+CoapplicantIncome = st.number_input('Coapplicant Income', min_value=0, step=1000)
+LoanAmount = st.number_input('Loan Amount', min_value=0, step=1000)
+Loan_Amount_Term = st.selectbox(
+    'Loan Amount Term', 
+    ['12 Months', '36 Months', '60 Months', '84 Months', '120 Months', 
+     '180 Months', '240 Months', '300 Months', '360 Months', '480 Months']
+)
+Credit_History = st.selectbox('Credit History (1=Good, 0=Bad)', [1, 0])
+Property_Area = st.selectbox('Property Area', ('Urban', 'Rural', 'Semiurban'))
 
-columns= ['Gender','Married','Dependents','Education','Self_Employed','ApplicantIncome','CoapplicantIncome',
-          'LoanAmount','Loan_Amount_Term','Credit_History','Property_Area']
+# Feature Mapping
+def preprocess_inputs():
+    term_mapping = {
+        '12 Months': 12, '36 Months': 36, '60 Months': 60, '84 Months': 84,
+        '120 Months': 120, '180 Months': 180, '240 Months': 240,
+        '300 Months': 300, '360 Months': 360, '480 Months': 480
+    }
+    data = {
+        'Gender': [1 if Gender == 'Male' else 0],
+        'Married': [1 if Married == 'Yes' else 0],
+        'Dependents': [Dependents],
+        'Education': [1 if Education == 'Graduate' else 0],
+        'Self_Employed': [1 if Self_Employed == 'Yes' else 0],
+        'ApplicantIncome': [ApplicantIncome],
+        'CoapplicantIncome': [CoapplicantIncome],
+        'LoanAmount': [LoanAmount],
+        'Loan_Amount_Term': [term_mapping[Loan_Amount_Term]],
+        'Credit_History': [Credit_History],
+        'Property_Area': [Property_Area]
+    }
+    return pd.DataFrame(data)
 
+# Prediction
 def predict():
-    col= np.array([Gender,Married,Dependents,Education,Self_Employed,
-                   ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area])
-    data= pd.DataFrame([col],columns=columns)
-    prediction= model.predict(data)[0]
-
-
+    input_data = preprocess_inputs()
+    
+    # Ensure input data matches model training format
+    input_data = pd.get_dummies(input_data)
+    prediction = model.predict(input_data)[0]
+    
     if prediction == 1:
-        st.success('You Can Get The Loan:thumbsup:')
+        st.success('You Can Get The Loan :thumbsup:')
     else:
-        st.error('Sorry You Cant Get The Loan:thumbsdown:')
+        st.error('Sorry, You Cannot Get The Loan :thumbsdown:')
 
-
-m = st.markdown("""
+# Button Styling
+st.markdown("""
 <style>
 div.stButton > button:first-child {
     background-color: #0099ff;
@@ -48,5 +74,5 @@ div.stButton > button:hover {
     }
 </style>""", unsafe_allow_html=True)
 
-
-st.button('Predict',on_click=predict)
+# Predict Button
+st.button('Predict', on_click=predict)
