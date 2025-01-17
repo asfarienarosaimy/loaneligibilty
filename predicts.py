9e192b578ece247a8e170944f00e8648183eb2c3
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2025-01-17 10:50:13 +0000

    update

--- predicts.py
+++ predicts.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+# Load the trained pipeline
+pipeline = joblib.load('rf_model_pipeline.pkl')
 
+# Extract available optiosn for categorical fetaures
+preprocessor = pipeline.named_steps['preprocessor']
+one_hot_encoder = preprocessor.named_transformers_['cat']
+categories = one_hot_encoder.categories_
