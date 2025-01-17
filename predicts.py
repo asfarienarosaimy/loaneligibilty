c1eb80a3510f09da172a42aa2d3053452ebef8c0
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2025-01-17 09:39:41 +0000

    update

---
+++ predicts.py
@@ -0,0 +1,15 @@
+import joblib
+import streamlit as st
+
+# Load the trained pipeline
+pipeline = joblib.load('rf_model_pipeline.pkl')
+
+# Extract available options for categorical features
+preprocessor = pipeline.named_steps['preprocessor']
+one_hot_encoder = preprocessor.named_transformers_['cat']
+categories = one_hot_encoder.categories_
+
+# Streamlit app
+st.title("Loan Status Prediction")
+st.caption('Fill the form below to start a prediction.')
+
